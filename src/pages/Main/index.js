dc0844ff07468bc995e993bccacbc941b3e37958
INDRA\dsenam <dsenam@indracompany.com>
2020-12-01 16:48:56 -0300

    + MainPage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, HeaderContainer, ArticleContainer, ListMovies } from './styles';
 
 function Main() {
   return (
       <>
-        <h1>Main</h1>
+        <Container>
+            <HeaderContainer>
+                <h1>React Movie</h1>
+                
+              <form>
+                <input type="text" placeholder="Digite o nome do filme" />
+              </form>
+
+              <button>Entrar</button>
+            </HeaderContainer>
+            <ArticleContainer>
+                <div>
+                    <img src="http://image.tmdb.org/t/p/w780/4ZocdxnOO6q2UbdKye2wgofLFhB.jpg" alt="Teste">
+
+                </img>
+                    <h1>Chick Fight</h1>
+
+                    <p>When Anna Wyncomb is introduced to an an underground, all-female fight club in order to turn the mess of her life around, she discovers she is much more personally connected to the history of the club than she could ever imagine.</p>
+                </div>
+            </ArticleContainer>
+
+            <ListMovies>
+              <h1>Filmes</h1>
+              <ul>
+                <li>
+                  <button>
+                    <img src="http://image.tmdb.org/t/p/w780/4ZocdxnOO6q2UbdKye2wgofLFhB.jpg" alt="Teste"/>
+                  </button>
+                </li>
+              </ul>
+            </ListMovies>
+        </Container>
       </>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -3,3 +3,103 @@ import styled from 'styled-components';
 export const Container = styled.div`
   
 `;
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  width: 100%;
+  height: 40px;
+  justify-content: space-between;
+  align-items: center;
+  background-color: black;
+  padding: 40px;
+
+  h1 {
+      color: white;
+  }
+
+  button {
+      background-color: transparent;
+      color: white;
+  }
+
+  form {
+    input {
+      margin-bottom: 30px;
+      margin-top: 20px;
+      width: 300px;
+      height: 30px;
+      border-radius: 7px;
+      
+      ::placeholder {
+          color: #DCDCDC;
+          padding: 10px;
+    }
+  }
+}
+`;
+
+export const ArticleContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    div {
+       position: relative;
+
+      img {
+        width: 100%;
+        height: 500px;
+      }
+
+      h1 {
+        position: absolute;
+        font-weight: bold;
+        font-size: 32px;
+        color: white;
+        left: 50px;
+        top: 320px;
+      }
+
+      p {
+        position: absolute;
+        font-size: 20px;
+        color: white;
+        left: 50px;
+        top: 375px;
+        margin-right: 20px;
+      }
+
+    }
+    
+`;
+
+export const ListMovies = styled.div`
+  padding: 40px;
+  
+
+  h1 {
+    margin-bottom: 10px;
+  }
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+
+  li {
+      list-style: none;
+      padding: 10px;
+
+      button {
+        :hover {
+          opacity: 0.8;
+        }
+      }
+    }
+
+  }
+
+  img {
+    height: 400px;
+    width: 280px;
+    border-radius: 5px;
+  }
+`;
